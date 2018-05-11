376f4c84fc29bf69790303a22caf17d121316c67
Alexander Kolbasov <akolb@cloudera.com>
2018-05-11 16:10:48 +0200

    Support partition subobjects

--- gometastore/hmsv2server/partition.go
+++ gometastore/hmsv2server/partition.go
@@ -94,12 +94,13 @@ func (s *metastoreServer) AddPartition(c context.Context,
 		if err != nil {
 			return err
 		}
+		log.Println("added partition", partition)
 
 		return nil
 	})
 
 	if err != nil {
-		log.Println("failed to create table:", err)
+		log.Println("failed to create partition:", err)
 		return &pb.AddPartitionResponse{
 			Sequence: req.Sequence,
 			Status: &pb.RequestStatus{
@@ -196,6 +197,33 @@ func (s *metastoreServer) GetPartition(c context.Context,
 		if err != nil {
 			return err
 		}
+
+		var table pb.Table
+
+		// Extract table
+		byNameBucket := dbBucket.Bucket([]byte(bynameHdr))
+		if byNameBucket == nil {
+			return fmt.Errorf("corrupt catalog %s/%s: no BYNAME info", catalog, dbName)
+		}
+		byIDBucket := dbBucket.Bucket([]byte(byIDHdr))
+		if byIDBucket == nil {
+			return fmt.Errorf("corrupt catalog %s/%s: no BYID info", catalog, dbName)
+		}
+		tblIDBytes := byNameBucket.Get([]byte(tableName))
+		if tblIDBytes == nil {
+			return fmt.Errorf("table %s:%s.%s does not exist", catalog, dbName, tableName)
+		}
+		tableData := byIDBucket.Get(tblIDBytes)
+		if tableData == nil {
+			return fmt.Errorf("catalog corrupted: table %s:%s.%s does not exist",
+				catalog, dbName, tableName)
+		}
+		if err = proto.Unmarshal(tableData, &table); err != nil {
+			return fmt.Errorf("catalog corruted: can't decode table data for %s.%s: %v",
+				dbName, tableName, err)
+		}
+
+
 		// Do we have this partition?
 		data := tablesBucket.Get([]byte(values))
 		if data == nil {
@@ -204,6 +232,7 @@ func (s *metastoreServer) GetPartition(c context.Context,
 		if err := proto.Unmarshal(data, &partition); err != nil {
 			return err
 		}
+		partition.Table = &table
 		return nil
 	})
 
@@ -242,6 +271,16 @@ func (s *metastoreServer) ListPartitions(req *pb.ListPartitionsRequest,
 		return fmt.Errorf("missing table name")
 	}
 
+	// Values that we are interested in
+	valuesMap := make(map[string]bool)
+
+	if req.Values != nil {
+		for _, values := range req.Values {
+			vString := strings.Join(values.GetValue(), "/")
+			valuesMap[vString] = true
+		}
+	}
+
 	err := s.db.View(func(tx *bolt.Tx) error {
 		dbBucket, err := getDatabaseBucket(tx, catalog, req.DbId)
 		if err != nil {
@@ -251,7 +290,36 @@ func (s *metastoreServer) ListPartitions(req *pb.ListPartitionsRequest,
 		if err != nil {
 			return err
 		}
-		tablesBucket.ForEach(func(k, v []byte) error {
+
+		// Extract table
+		byNameBucket := dbBucket.Bucket([]byte(bynameHdr))
+		if byNameBucket == nil {
+			return fmt.Errorf("corrupt catalog %s/%s: no BYNAME info", catalog, dbName)
+		}
+		byIDBucket := dbBucket.Bucket([]byte(byIDHdr))
+		if byIDBucket == nil {
+			return fmt.Errorf("corrupt catalog %s/%s: no BYID info", catalog, dbName)
+		}
+		tblIDBytes := byNameBucket.Get([]byte(tableName))
+		if tblIDBytes == nil {
+			return fmt.Errorf("table %s:%s.%s does not exist", catalog, dbName, tableName)
+		}
+		data := byIDBucket.Get(tblIDBytes)
+		if data == nil {
+			return fmt.Errorf("catalog corrupted: table %s:%s.%s does not exist",
+				catalog, dbName, tableName)
+		}
+
+		var table pb.Table
+		err = proto.Unmarshal(data, &table)
+		if err != nil {
+			return fmt.Errorf("catalog corruted: can't decode table data for %s.%s: %v",
+				dbName, tableName, err)
+		}
+
+		first := true
+
+		walker := func(k, v []byte) error {
 			partition := new(pb.Partition)
 			if err := proto.Unmarshal(v, partition); err != nil {
 				return nil
@@ -267,6 +335,33 @@ func (s *metastoreServer) ListPartitions(req *pb.ListPartitionsRequest,
 						part.Parameters = partition.Parameters
 					case "values":
 						part.Values = partition.Values
+					case "sd":
+						part.Sd = partition.Sd
+					case "sd.parameters":
+						if part.Sd == nil {
+							part.Sd = &pb.StorageDescriptor{Parameters: partition.Sd.Parameters}
+							log.Println("sd = ", part.Sd, partition.Sd.Parameters)
+						} else {
+							part.Sd.Parameters = partition.Sd.Parameters
+						}
+					case "sd.serdeinfo":
+						if part.Sd == nil {
+							part.Sd = &pb.StorageDescriptor{SerdeInfo: partition.Sd.SerdeInfo}
+						} else {
+							part.Sd.SerdeInfo = partition.Sd.SerdeInfo
+						}
+					case "sd.serdeinfo.parameters":
+						if part.Sd == nil {
+							part.Sd = &pb.StorageDescriptor{SerdeInfo: &pb.SerDeInfo{Parameters: partition.Sd.SerdeInfo.Parameters}}
+						} else {
+							part.Sd.SerdeInfo.Parameters = partition.Sd.SerdeInfo.Parameters
+						}
+					case "table":
+						if first {
+							// Include table in first partition only
+							first = false
+							part.Table = &table
+						}
 					}
 				}
 				log.Println("send", part)
@@ -275,13 +370,33 @@ func (s *metastoreServer) ListPartitions(req *pb.ListPartitionsRequest,
 					return err
 				}
 			} else {
+				if first {
+					// Include table in first partition only
+					first = false
+					partition.Table = &table
+				}
+				log.Println("Send partition", partition)
 				if err := stream.Send(partition); err != nil {
 					log.Println("err sending:", err)
 					return err
 				}
 			}
 			return nil
-		})
+		}
+
+		if len(valuesMap) == 0 {
+			tablesBucket.ForEach(walker)
+		} else {
+			// Walk over values only
+			for value := range valuesMap {
+				v := tablesBucket.Get([]byte(value))
+				if v == nil {
+					continue
+				}
+				walker([]byte(value), v)
+			}
+		}
+
 		return nil
 	})
 
@@ -314,7 +429,7 @@ func (s *metastoreServer) DropPartitions(c context.Context,
 	if tableName == "" {
 		return nil, fmt.Errorf("missing table name")
 	}
-	partitionValues := req.GetValues();
+	partitionValues := req.GetValues()
 
 	err := s.db.Update(func(tx *bolt.Tx) error {
 		dbBucket, err := getDatabaseBucket(tx, catalog, req.DbId)
